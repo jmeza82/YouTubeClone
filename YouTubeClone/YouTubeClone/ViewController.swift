5c073d9a58b9b829a89014d83041bbed0a510327
Juan Meza <juanmeza255@hotmail.com>
2024-10-19 14:55:09 -0600

    [FEAT: Add Images, Icons, Color, and create Launscreen]

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by MMJ on 19/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
